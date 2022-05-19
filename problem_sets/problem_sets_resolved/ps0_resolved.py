85f7bd8dd392ca5340de462bbadaef553d121fb9
Raphael Lucas Mendes <rapha__mendes@hotmail.com>
2022-05-18 21:46:32 -0300

    completed Lecture 2

---
+++ problem_sets/problem_sets_resolved/ps0_resolved.py
@@ -0,0 +1,14 @@
+import math
+
+x = float(input("Enter number x: ")) 
+y = float(input("Enter number y: ")) 
+
+
+x_pow_y = x**y
+
+log_x = math.log(x,2)
+
+
+
+print(f"X**y =  {x_pow_y}") 
+print(f"log(x) = {log_x}") 
